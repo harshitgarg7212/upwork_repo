64f610edd66ccf3c00e19275690fe181c8881c9a
Nazar Tolstoy <nazartolstoy@MacBook-Air-Nazar.local>
2024-04-20 14:57:22 +0300

    add theme && fix header responsive

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { Outlet } from "react-router-dom";
 
-import './App.css';
-import Header from "./components/Header";
+import "./App.css";
+
 import Footer from "./components/Footer";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "theme";
+import Header from "components/header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <div className="header-wrapper">
-        <Header />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <div className="header-wrapper">
+          <Header />
+        </div>
+        <div className="content-wrapper">
+          <Outlet />
+        </div>
+        <Footer />
       </div>
-      <div className="content-wrapper">
-        <Outlet />
-      </div>
-      <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
