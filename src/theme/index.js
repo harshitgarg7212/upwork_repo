2588df4fa4ff23eaa7a82891cb8a8c9ec68f2235
Nazar Tolstoy <nazartolstoy@MacBook-Air-Nazar.local>
2024-04-22 10:39:26 +0300

    fix modal && section title

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,7 +1,11 @@
 import { createTheme } from "@mui/material";
 
 import { breakpoints } from "./foundation";
+import { Modal } from "./components/modal";
 
 export const theme = createTheme({
   breakpoints,
+  components: {
+    MuiModal: Modal,
+  },
 });
